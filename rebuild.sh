2e6a2e6b63100c1d2d0302b46a49d07d15ee61d1
nrd-lab-tester <molybdeunummb@gmail.com>
2022-06-01 00:04:21 -0600

    removed deploy

--- rebuild.sh
+++ rebuild.sh
@@ -5,10 +5,6 @@ function redeploy(){
     str=($(rclone config file));loc=${str[5]};cp $loc ./rclone.conf;
     echo $(fly deploy --remote-only)
 }
-function deploy(){
-    str=($(rclone config file));loc=${str[5]};cp $loc ./rclone.conf;
-    echo $(fly launch --remote-only)
-}
 function install(){
     echo $(curl https://rclone.org/install.sh | sudo sh)
     echo $(curl -L https://fly.io/install.sh | sudo sh)
